50313df881be97b7d7dab3495c045b4624e4f915
Juan Du <jdu@jdu.local>
2015-05-31 17:13:17 +0800

    add onChange method for input

--- app/scripts/Login.jsx
+++ app/scripts/Login.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
 
+import LoginAction from './actions/LoginAction'
+
 const Login = React.createClass({
+    getInitialState() {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+
     render() {
         return (
             <form ref="form">
-                <input type="text" name="username" placeholder="Username" autofocus = "autofocus" />
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit" name="submit"> Login </button>
+                <input
+                    ref="username"
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    autofocus = "autofocus"
+                    value={this.state.username}
+                    onChange={this.onUsernameChange}
+                />
+                <input
+                    ref="password"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.onPasswordChange}
+                />
+                <button
+                    ref="button"
+                    type="submit"
+                    name="submit">
+                    Login
+                </button>
             </form>
         );
+    },
+
+    onUsernameChange(e) {
+        this.setState({username: e.target.value});
+    },
+
+    onPasswordChange(e) {
+        this.setState({password: e.target.value});
+    },
+
     }
 });
 
--- test/LoginSpecs.js
+++ test/LoginSpecs.js
@@ -1,26 +1,41 @@
 import should from 'should';
 import sinon from 'sinon';
 import React from 'react/addons';
+
 import Login from '../app/scripts/Login'
 
 const TestUtils = React.addons.TestUtils;
 
 describe('Login', function() {
+    let LoginComponent, username, password, button;
+
+    beforeEach(() => {
+        LoginComponent = TestUtils.renderIntoDocument(<Login/>);
+        username = LoginComponent.refs.username;
+        password = LoginComponent.refs.password;
+        button = LoginComponent.refs.button;
+    });
+
     context('render', () => {
-        let LoginComponent;
-
-        beforeEach(() => {
-            LoginComponent = TestUtils.renderIntoDocument(<Login/>);
-        });
-
         it('should have username, password and button', () => {
-            const username = LoginComponent.refs.form.props.children[0];
-            const password = LoginComponent.refs.form.props.children[1];
-            const button = LoginComponent.refs.form.props.children[2];
-
             username.props.name.should.be.eql('username');
             password.props.name.should.be.eql('password');
             button.props.name.should.be.eql('submit');
         });
     });
+
+    context('#onUsernameChange', () => {
+        it('should get username value when input change', () => {
+            TestUtils.Simulate.change(username.getDOMNode(), {target: {value: 'test'}});
+            LoginComponent.state.username.should.be.eql('test');
+        });
+    });
+
+    context('#onPasswordChange', () => {
+        it('should get password value when input change', () => {
+            TestUtils.Simulate.change(password.getDOMNode(), {target: {value: 'test'}});
+            LoginComponent.state.password.should.be.eql('test');
+        });
+    });
+
 });
